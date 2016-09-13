87b89a017bc2570337238ed64682fd3397825139
Jacob Rosenberg <jake@lendup.com>
2016-09-12 21:24:20 -0700

    change to a tts system

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 var express = require('express');
 var app = express();
 
+var say = require('say');
+
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json()); // for parsing application/json
+
 // reply to request with "Hello World!"
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.post('/speak', function (req, res, next) {
+  console.log("Speaking " + req.body.message);
+  say.speak(req.body.message);
+  res.sendStatus(200);
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
-
+var server = app.listen(7388, function () {
   var port = server.address().port;
   console.log('Example app listening on port ', port);
-
 });
