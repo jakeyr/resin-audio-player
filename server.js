0c7c9f1a21dbea1db91a907084d7efd259f0ad70
Jacob Rosenberg <jake@lendup.com>
2016-09-19 17:02:33 -0700

    adding ability to play wavs

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var express = require('express');
-var app = express();
-
-var say = require('say');
-
+var express    = require('express');
+var http       = require('http');
+var say        = require('say');
 var bodyParser = require('body-parser');
+var fs         = require('fs');
+var crypto     = require('crypto');
+var play       = require('play');
+
+var app = express();
 
 app.use(bodyParser.json()); // for parsing application/json
 
@@ -11,6 +14,30 @@ app.use(bodyParser.json()); // for parsing application/json
 app.post('/speak', function (req, res, next) {
   console.log("Speaking " + req.body.message);
   say.speak(req.body.message);
+  res.sendStatus(200);
+});
+
+app.post('/play', function (req, res, next) {
+  console.log("Playing " + req.body.url);
+
+  var fileName = "/tmp/" + crypto.createHash('md5').update(req.body.url).digest("hex") + ".wav";
+
+  fs.exists(fileName, function(exists) {
+  	if (exists) {
+  	  console.log("Playing local cached file " + fileName);
+  	  play.sound(fileName);
+  	} else {
+	  console.log("Downloading to " + fileName);
+	  var file = fs.createWriteStream(fileName);
+	  var request = http.get(req.body.url, function(response) {
+  		response.pipe(file);
+	  	response.on('end', function () {
+	      play.sound(fileName);
+	  	});
+  	  });
+  	}
+  });
+
   res.sendStatus(200);
 });
 
